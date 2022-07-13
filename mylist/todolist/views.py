e233f4c2805b7060f59449e5e5061ac6c7e9201b
sreenu1996 <srinuuppuluri108@gmail.com>
2022-07-13 22:12:00 +0530

    1st commit in sree branch

--- mylist/todolist/views.py
+++ mylist/todolist/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+from todolist.models import Todolist
+
 # Create your views here.
 def index (request):
-    return render(request,'index.html')
+    todolist=Todolist.objects.order_by('id')
+    content={'todolist':todolist}
+    return render(request,'index.html',content)
